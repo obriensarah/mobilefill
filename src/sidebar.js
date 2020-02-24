129b223266f25f987eeae46960f01b0b5d618974
aiaiJ <ajin2@dons.usfca.edu>
2020-02-24 17:17:09 -0600

    .

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
+import Button from 'react-bootstrap/Button';
+import {List, XSquare, ChevronLeft, InfoSquare} from 'react-bootstrap-icons'
+
 
 export default props => {
   return (
     // Pass on our props
-    <Menu {...props}>
+    <Menu customBurgerIcon={ <Button variant="secondary">
+          <List color="ghostwhite" size={25}/></Button> } >
       <a className="menu-item" href="/">
         Disclaimer
       </a>
